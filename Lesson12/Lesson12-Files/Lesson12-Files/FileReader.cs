332299a31fd02f3f0105e1df875aa5cadf0af871
Bogdan <BogdanSnitsaruk@gmail.com>
2016-04-07 00:42:09 +0300

    Without Specification

---
+++ Lesson12/Lesson12-Files/Lesson12-Files/FileReader.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson12_Files
+{
+    public class FileReader
+    {
+        private const string inputFile  = "Input.txt";
+        private const string blackFile  = "Blacklist.txt";
+        private const string whiteFile  = "Whitelist.txt";
+
+
+        List<string> inputList = new List<string>();
+        List<string> blackList = new List<string>();
+        List<string> whiteList = new List<string>();
+
+      
+
+        // Common method for reading from files and put info to List<string> Collection
+        private void ReadFromFile (string filePath, List<string> outputList)
+        {
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                string line;
+
+                while ((line = reader.ReadLine()) != null)
+                {
+                    outputList.Add(line);
+                    Console.WriteLine(line);
+                }
+            }
+        }
+
+        // Common method for writing to files 
+        public void WriteToFile(string filePath, string result)
+        {
+            using (StreamWriter writer = new StreamWriter(filePath,true))
+            {
+                writer.WriteLine(result);
+                
+            }
+        }
+
+        // Reading Input.txt file
+        public List<string> ReadInput()
+        {
+            ReadFromFile(inputFile, inputList);
+            return inputList;
+        }
+
+        // Reading WhiteList.txt file
+        public List<string> ReadWhiteList()
+        {
+            ReadFromFile(whiteFile, whiteList);
+            return whiteList;
+        }
+
+        // Reading BlackList.txt file
+        public List<string> ReadBlackList()
+        {
+            ReadFromFile(blackFile, blackList);
+            return blackList;
+        }
+    }
+}
---
+++ Lesson12/Lesson12-Files/Lesson12-Files/Filters.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson12_Files
+{
+    public class Filters
+    {
+        private string tempResult = null;
+       
+
+        FileReader fr = new FileReader();
+
+        public void WhiteListFilter()
+        {
+            const string outputFile = "Output_whitelist.txt";
+            
+            List<string> white = fr.ReadWhiteList();
+            
+            for (int i = 0; i < white.Count; i++)
+            {
+                tempResult = white[i];
+                fr.WriteToFile(outputFile, tempResult);
+            }
+        }
+
+        public void BlackListFilter()
+        {
+            const string outputFile = "Output_blacklist.txt";
+
+            List<string> input = fr.ReadInput();
+            List<string> black = fr.ReadBlackList();
+                                    
+            for (int i = 0; i < input.Count; i++)
+            {
+                tempResult = input[i];
+                if (!black.Contains(tempResult))
+                {
+                    fr.WriteToFile(outputFile, tempResult);
+                }
+            }
+        }
+
+        public void CombinationFilter()
+        {
+            const string outputFile = "Output_blacklist_whitelist.txt";
+            List<string> input = fr.ReadInput();
+            List<string> black = fr.ReadBlackList();
+            List<string> white = fr.ReadWhiteList();
+            
+
+            for (int i = 0; i < input.Count; i++)
+            {
+                tempResult = input[i];
+                if (!black.Contains(tempResult))
+                {
+                    if (white.Contains(tempResult))
+                    {
+                        fr.WriteToFile(outputFile, tempResult);
+                    }
+                }
+            }
+
+        }
+
+       
+    }
+}
